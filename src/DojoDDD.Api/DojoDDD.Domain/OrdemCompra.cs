c2229384c51e1d05cb57199aef43d0c5484e60ba
diego.silva <diego.silva@TIT-1154>
2019-03-26 16:15:17 -0300

    Commit Inicial

---
+++ src/DojoDDD.Api/DojoDDD.Domain/OrdemCompra.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace DojoDDD.Api.DojoDDD.Domain
+{
+    public class OrdemCompra
+    {
+        public OrdemCompra()
+        {
+            Status = OrdemCompraStatus.Solicitado;
+        }
+
+        public int Id { get; set; }
+        public DateTime DataOperacao { get; set; }
+        public int ProdutoId { get; set; }
+        public string ClienteId { get; set; }
+        public decimal ValorOperacao { get; set; }
+        public OrdemCompraStatus Status { get; set; }
+    }
+}
---
+++ src/DojoDDD.Api/DojoDDD.Domain/Produto.cs
@@ -0,0 +1,11 @@
+﻿namespace DojoDDD.Api.DojoDDD.Domain
+{
+    public class Produto
+    {
+        public int Id { get; set; }
+        public string Descricao { get; set; }
+        public decimal Estoque { get; set; }
+        public decimal ValorUnitario { get; set; }
+        public int ValorMinimoDeCompra { get; set; }
+    }
+}
